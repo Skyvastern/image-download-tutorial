74d572bbc7f131aeefa4f9dad9455dbeda73a83f
Arpit Srivastava <skyvastern@gmail.com>
2025-02-01 20:50:08 +0530

    Add optional scene for downloading DALLE generated images

---
+++ optional_image_generation/image_generate_api/image_generate_api.gd
@@ -0,0 +1,34 @@
+extends HTTPRequest
+class_name ImageGenerateAPI
+
+signal processed
+var url: String = "https://api.openai.com/v1/images/generations"
+
+
+func _ready() -> void:
+	request_completed.connect(_on_request_completed)
+
+
+func make_request(prompt: String) -> void:
+	var headers: PackedStringArray = [
+		"Content-Type: application/json",
+		"Authorization: Bearer " + ENV.OPENAI_API_KEY
+	]
+	
+	var body: String = JSON.stringify({
+		"model": "dall-e-3",
+		"prompt": prompt,
+		"n": 1,
+		"size": "1024x1024"
+	})
+	
+	request(url, headers, HTTPClient.METHOD_POST, body)
+
+
+func _on_request_completed(result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
+	var json: Dictionary = {}
+	
+	if result == 0:
+		json = JSON.parse_string(body.get_string_from_utf8())
+	
+	processed.emit(result, response_code, json)
---
+++ optional_image_generation/optional_image_generation.gd
@@ -0,0 +1,47 @@
+# In some places I have written code in not so great way
+# 1. Directly using methods of image_web.image_status doesn't feel right
+# 2. Awaiting image_web.image_download_api.processed doesn't feel right too
+#
+# But for the scope of this demonstration of image download, I think its completely fine
+
+extends Control
+class_name OptionalImageGeneration
+
+@export_group("UI")
+@export var image_web: ImageWeb
+@export var prompt_input: LineEdit
+@export var generate_btn: Button
+
+@export_group("References")
+@export var image_generate_api: ImageGenerateAPI
+
+
+func _ready() -> void:
+	generate_btn.pressed.connect(_on_generate_btn_pressed)
+	image_generate_api.processed.connect(_on_image_generated)
+
+
+func _on_generate_btn_pressed() -> void:
+	image_web.image_status.show_status("Generating image...")
+	prompt_input.editable = false
+	generate_btn.disabled = true
+	
+	var prompt: String = prompt_input.text
+	image_generate_api.make_request(prompt)
+
+
+func _on_image_generated(result: int, response_code: int, json: Dictionary) -> void:
+	if result != 0 or response_code != 200:
+		image_web.image_status.show_error("An error occured generating image.")
+		prompt_input.editable = true
+		generate_btn.disabled = false
+	
+	else:
+		image_web.image_status.hide_status()
+		
+		var image_url: String = json["data"][0]["url"]
+		image_web.download_image(image_url)
+		
+		await image_web.image_download_api.processed
+		prompt_input.editable = true
+		generate_btn.disabled = false
