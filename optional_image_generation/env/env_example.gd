408460ba86e008dbc37f1e58a1a10821be96fc5f
Arpit Srivastava <skyvastern@gmail.com>
2025-02-16 23:15:40 +0530

    Remove optional image generation example

--- optional_image_generation/env/env_example.gd
+++
@@ -1,8 +0,0 @@
-# 1. Duplicate this file, and name new one - "env.gd"
-# 2. Set env.gd class name to - "ENV"
-# 3. In env.gd, fill in the variables with your own values
-
-extends Node
-class_name ENV_EXAMPLE
-
-static var OPENAI_API_KEY: String = ""
